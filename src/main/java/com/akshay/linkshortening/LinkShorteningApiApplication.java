1579d63282e4e5fce8b80735eabeb63d49fe421a
Akshay <akshaychaudhary29@gmail.com>
2022-04-23 08:47:31 +0530

    Initial Commit

---
+++ src/main/java/com/akshay/linkshortening/LinkShorteningApiApplication.java
@@ -0,0 +1,13 @@
+package com.akshay.linkshortening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkShorteningApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkShorteningApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/akshay/linkshortening/LinkShorteningApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akshay.linkshortening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinkShorteningApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
